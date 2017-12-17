5a26edef978e018069c046d1c634350afef0de0b
wrnice <wrnice@pmail.neomailbox.net>
2017-12-17 22:43:23 +0100

    rebuilt again

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 var webpack = require('webpack');
 var isProd = (process.env.NODE_ENV === 'production');
 
-// module.exports = [{
-  module.exports = {
+module.exports = [{
   node: {fs: 'empty'},
   entry: './src/index.js',
   devtool: 'source-map',
@@ -20,47 +19,46 @@ var isProd = (process.env.NODE_ENV === 'production');
         query: {
           presets: ['react', 'es2015']
         }
-//       }
-//     ]
-//   }
-// }, {
-//   node: {fs: 'empty'},
-//   entry: './src/index.js',
-//   devtool: 'source-map',
-//   output: {
-//     path: './dist',
-//     filename: isProd ? 'simplemde-editor.min.js' : 'simplemde-editor.js',
-//     libraryTarget: 'umd'
-//   },
-//   plugins: [
-//     new webpack.ProvidePlugin({
-//       React: "React", react: "React", "window.react": "React", "window.React": "React"
-//     }),
-//   ],
-//   resolve: {
-//     extensions: ['', '.js'],
-//     alias: {
-//       "react": "src/dummyReact.js"
-//     }
-//   },
-//   externals: {
-//     // Use external version of React
-//     "react": "React"
-//   },
-//   module: {
-//     noParse: ["react"],
-//     loaders: [
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         loader: "babel",
-//         query: {
-//           presets: ['react', 'es2015']
-//         }
-       }
-     ]
-   }
-//
-// }
-// ];
-};
+      }
+    ]
+  }
+}, {
+  node: {fs: 'empty'},
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: './dist',
+    filename: isProd ? 'simplemde-editor.min.js' : 'simplemde-editor.js',
+    libraryTarget: 'umd'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: "React", react: "React", "window.react": "React", "window.React": "React"
+    }),
+  ],
+  resolve: {
+    extensions: ['', '.js'],
+    alias: {
+      "react": "src/dummyReact.js"
+    }
+  },
+  externals: {
+    // Use external version of React
+    "react": "React"
+  },
+  module: {
+    noParse: ["react"],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel",
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+
+}
+];
